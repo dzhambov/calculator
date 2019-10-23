95ff5dadb011b058a9769f289bb8934418766967
Student <example@example.com>
2019-10-23 11:04:52 -0700

    add subtract function to the calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -20,14 +20,31 @@ return number1 / number2;
 
 
 $(document).ready(function(){
+
   $("form#add").submit(function(event){
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").slideDown().text(result);
+    $("#add-output").text(result);
+});
+
+$(document).ready(function(){
+
+
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#subtract-output").text(result);
+  });
 
 });
+
+
+
+
 
 
 
